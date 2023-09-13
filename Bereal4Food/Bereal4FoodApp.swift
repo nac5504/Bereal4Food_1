6793743968d78f213f0d39694d1bc95ef2cdc4ef
nac5504 <nac5504@gmail.com>
2023-09-12 20:54:51 -0400

    Initial Commit

---
+++ Bereal4Food/Bereal4FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bereal4FoodApp.swift
+//  Bereal4Food
+//
+//  Created by Nicholas Candello on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Bereal4FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
